40ac63b8eccd0257aad762c7c803072a3d4e123d
Abigail Moran <amoran@rustyamd1.cm.cluster>
2026-01-21 15:48:28 -0500

    updated mock obs

--- src/mock_observations.py
+++ src/mock_observations.py
@@ -20,8 +20,8 @@ class Uncertainties(object):
     @classmethod
     def from_snr(cls, snr):
         # These formulas come from estimations based on uncertainties in GWTC-3
-        slmc = 0.05*20/snr    
-        slq = .1*20/snr #random number standing in for sigma log q rn
+        slmc =0# 0.05*20/snr    
+        slq = 0#.1*20/snr #random number standing in for sigma log q rn
         sld =  0#.2*20/snr #.2, .00001* is the error on the small dl samples
 
         return cls(slmc, slq, sld)
