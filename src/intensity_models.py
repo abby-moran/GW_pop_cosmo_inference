030a17178817cdc07c8d4d0c59a00e2896d6dd9b
Abigail Moran <amoran@rusty1.cm.cluster>
2026-03-25 17:27:33 -0400

    Updated intensity models. pdraw is now log_pdraw in the inputs

--- src/intensity_models.py
+++ src/intensity_models.py
@@ -223,9 +223,9 @@ class LogDNDM(object):
         log_dNdmbhmax_at_samples = self.interp_2d_dndmpisn(mbhmax_at_samples, z)
         log_high_mass_tail = -self.c*jnp.log(jnp.divide(m, mbhmax_at_samples))    
         log_dNdm = jnp.logaddexp(log_dNdm, jnp.log(self.fpl) + log_dNdmbhmax_at_samples + log_high_mass_tail  + log_smooth_turnon(m, mbhmax_at_samples))
-        #log_dNdm = jnp.where(m < self.mbh_min, np.NINF, log_dNdm)
-        logwindow = mmin_log_smooth_turnon(m, delta_m=self.delta_m, mmin= self.mbh_min)
-        log_dNdm = log_dNdm + logwindow
+        log_dNdm = jnp.where(m < self.mbh_min, -np.inf, log_dNdm)
+        #logwindow = mmin_log_smooth_turnon(m, delta_m=self.delta_m, mmin= self.mbh_min)
+        log_dNdm = log_dNdm# + logwindow
         return log_dNdm 
 
 @dataclass
@@ -299,7 +299,6 @@ class LogDNDMDQDV(object):
 
         m2 = q*m1
         mt = m1+m2
-
         return self.log_dndm(m1, z) + self.log_dndm(m2, z) + self.beta*jnp.log(mt/(self.mref*(1 + self.qref))) + jnp.log(m1) + self.log_dndv(z) - self.log_norm
 
 @dataclass
@@ -377,6 +376,12 @@ def get_deterministic_parameters(sample):
     mbhmax = numpyro.deterministic('mbhmax', sample['mpisn'] + sample['dmbhmax'])   
     return dict(kappa=kappa, fpl=fpl, mbhmax=mbhmax)
 
+def log_smooth_neff_boundary(values, criteria):
+        scaled_x = (values - criteria) / (0.05 * criteria)
+        # Linear ramp below threshold: gradient is bounded to 1 everywhere,
+        # preventing the step-size collapse that power-4 (gradient ~32000 at
+        # scaled_x=-20) and power-10 (gradient ~5e12) caused.
+        return jnp.minimum(0.0, scaled_x)
 
 def build_population_model(sample):
     return LogDNDMDQDV(
@@ -388,14 +393,14 @@ def build_population_model(sample):
     )
 
     
-def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel, Ndraw, priors=None):
+def pop_cosmo_model(m1s_det, qs, dls, log_pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel, Ndraw, priors=None):
     """
     Ndraw is # of events in the injection samples used to estimate the selection function
     """
-    m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel = map(jnp.array, (m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel))
+    m1s_det, qs, dls, log_pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel = map(jnp.array, (m1s_det, qs, dls, log_pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel))
 
     log_pdraw_sel = jnp.log(pdraw_sel)
-    log_pdraw = jnp.log(pdraw)
+    #log_pdraw = jnp.log(pdraw)
     nobs = m1s_det.shape[0]
     nsamp = m1s_det.shape[1]
 
@@ -414,8 +419,8 @@ def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw
     # event-wise log weight for deteced events = log of pop density (model's differential merger rate density) 
     #      + Jacobian from source to detector frame 
     #      + cosmology factors (for volume element, D_L to z) + log_pdraw (corrects for non-uniform prior over parameter space)
-    log_wts = log_dN(m1s, qs, zs) - 2*jnp.log1p(zs) + jnp.log(cosmo.dVCdz(zs)) - jnp.log(cosmo.ddL_dz(zs)) - log_pdraw
-
+    log_wts = log_dN(m1s, qs, zs)  - log_pdraw -2*jnp.log1p(zs)+ jnp.log(cosmo.dVCdz(zs))  - jnp.log(cosmo.ddL_dz(zs)) 
+    # dLdz/1+z is to deal with pdraw being in detector frame mass, dL
     log_like_per_event = jss.logsumexp(log_wts, axis=1) - jnp.log(nsamp)  # shape (nobs,)
 
     log_like_per_event = jnp.nan_to_num(log_like_per_event, nan=-1e30, posinf=1e30, neginf=-1e30)
@@ -428,21 +433,37 @@ def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw
     m1s_sel = m1s_det_sel / (1 + zs_sel)
 
     # now get weights for injected events as with log_wts
-    log_sel_wts = log_dN(m1s_sel, qs_sel, zs_sel) - 2*jnp.log1p(zs_sel) + jnp.log(cosmo.dVCdz(zs_sel)) - jnp.log(cosmo.ddL_dz(zs_sel)) - log_pdraw_sel
-    log_mu_sel = jss.logsumexp(log_sel_wts) - jnp.log(Ndraw)
-    _ = numpyro.factor('selfactor', jnp.nan_to_num(jnp.nan_to_num(-nobs*log_mu_sel, nan=-np.inf)))
+    log_sel_wts =  log_dN(m1s_sel, qs_sel, zs_sel)  - log_pdraw_sel- 2*jnp.log1p(zs_sel) + jnp.log(cosmo.dVCdz(zs_sel)) - jnp.log(cosmo.ddL_dz(zs_sel))
+    #make sure that m1s_sel is in source frame, and pdraw_sel is in det mass frame
+    log_mu_sel =  jss.logsumexp(log_sel_wts) - jnp.log(Ndraw)
+    _ = numpyro.factor('selfactor', jnp.nan_to_num(jnp.nan_to_num(-nobs*log_mu_sel, nan=-np.inf))) #fix the np.inf if this is throwing the errors during inittialization
 
     log_mu2 = jss.logsumexp(2*log_sel_wts) - 2*jnp.log(Ndraw)
     log_s2 = log_mu2 + jnp.log1p(-jnp.exp(2*log_mu_sel - jnp.log(Ndraw) - log_mu2))
-    _ = numpyro.deterministic('neff_sel', jnp.exp(2*log_mu_sel - log_s2))
+    
+    #_ = numpyro.deterministic('neff_sel', jnp.exp(2*log_mu_sel - log_s2))
+    #mu_sel = jnp.exp(log_mu_sel)
+    # get some derived quantities for later
+    # effective sample size for weights
+    #_ = numpyro.deterministic('neff', jnp.exp(2*jss.logsumexp(log_wts, axis=1) - jss.logsumexp(2*log_wts, axis=1)))
+    
+    #N eff cuts
+    neff = jnp.exp(2 * jss.logsumexp(log_wts, axis=1) - jss.logsumexp(2 * log_wts, axis=1))
+    min_neff = jnp.min(neff)
+    numpyro.deterministic("neff", neff)
+    numpyro.factor("neff_criteria",jnp.nan_to_num(log_smooth_neff_boundary(min_neff, nobs), neginf=-1e20, posinf=1e20),)
+
+    #ratio = jnp.exp(2 * log_mu_sel - jnp.log(Ndraw) - log_mu2)
+    #ratio = jnp.clip(ratio, a_min=0.0, a_max=1.0 - 1e-12)
+    #log_s2 = log_mu2 + jnp.log1p(-ratio)
+    neff_sel = jnp.exp(2 * log_mu_sel - log_s2)
+    numpyro.deterministic("neff_sel", neff_sel)
+    numpyro.factor("neff_sel_criteria",jnp.nan_to_num(log_smooth_neff_boundary(neff_sel, 4 * nobs), neginf=-1e20, posinf=1e20), )
+    #mu_sel = jnp.exp(log_mu2)
     mu_sel = jnp.exp(log_mu_sel)
 
     R_unit = numpyro.sample('R_unit', dist.Normal(0, 1))
     R = numpyro.deterministic('R', nobs/mu_sel + jnp.sqrt(nobs)/mu_sel*R_unit)
-
-    # get some derived quantities for later
-    # effective sample size for weights
-    _ = numpyro.deterministic('neff', jnp.exp(2*jss.logsumexp(log_wts, axis=1) - jss.logsumexp(2*log_wts, axis=1)))
 
     # differential merger rate with respect to mass, at fixed reference values of mass ratio, z
     _ = numpyro.deterministic('mdNdmdVdt_fixed_qz', coords['m_grid']*R*jnp.exp(log_dN(coords['m_grid'], log_dN.qref, log_dN.zref)))
