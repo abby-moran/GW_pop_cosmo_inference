4937665d64d1bb2b0df6d573cde43cadae66e8ce
Abigail Moran <amoran@rusty2.cm.cluster>
2026-04-06 17:02:43 -0400

    moved some files out of the reporduce dir. Staging that one to be deleted in future versions

---
+++ scripts/call_inf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#SBATCH -p gpu
+#SBATCH --gpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --time=0-20
+
+module load cuda/12 cudnn
+
+source ~/miniforge3/bin/activate GW_Bump
+unset LD_LIBRARY_PATH
+
+cd /mnt/home/amoran/GW_pop_cosmo_inference/reproduce/
+
+srun python run_inf.py
