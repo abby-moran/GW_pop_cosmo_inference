Fixed the factor of ndet we were off by in weighting. Also changed rho to be drawn from a truncated normal (with proper reweighting) since with more detectors we're getting issues with negative distances more often

scripts/gen_inj.py
@@ -2,7 +2,6 @@ from astropy.cosmology import Planck18
 import astropy.units as u
 import warnings
 warnings.filterwarnings("ignore", "Wswiglal-redir-stdio")
-import lal
 import sys
 sys.path.append('../src/')
 import lalsimulation as lalsim
@@ -19,10 +18,8 @@ import argparse
 from pathlib import Path
 import shutil
 import subprocess
-from datetime import datetime
 import os
 import jax
-import h5py
 import jax.scipy.special as jss
 jax.config.update("jax_enable_x64", True)
 
@@ -157,7 +154,7 @@ if __name__ == "__main__":
     dL_fid   = float(grid["dL_fid"])
     
     log_snr_interp = RegularGridInterpolator((m1_grid, q_grid), np.log(snr_grid), bounds_error=False, fill_value=-np.inf)
-    num_loops=1
+    num_loops=10
     for i in range(num_loops):
         ndraw=int(1e7)
         zpdf = scipy.stats.uniform(loc=0, scale=population_parameters["zmax"])        

scripts/reweight_inj.py
@@ -56,7 +56,7 @@ nsamples = run.getint("n_PE")
 obs_file = os.path.join(run_dir, cfg["run"]["obs_file"])
 pe_file = os.path.join(run_dir, cfg["run"]["output_file_PE"])
 sel_file = os.path.join(run_dir, cfg["run"]["output_sel_file"])
-ndet = 3 #run.getint("ndet")
+ndet =run.getint("ndet")
 
 write_obs = run.getboolean("write_obs")
 new_sel = run.getboolean("new_sel")
@@ -91,9 +91,10 @@ def get_mock_obs(df, out_file, cosmo, detection_threshold=8,
 
     #jitter = detection_rng.normal(loc=0, scale=np.sqrt(ndet), size=len(df))    
     if jitter_SNR:
-        df['SNR_OBS'] = norm.rvs(loc=df['SNR'], scale= np.sqrt(ndet),  random_state=noise_rng)
+        a_rho=(0.0 - df['SNR']) / np.sqrt(ndet)
+        df['SNR_OBS'] = truncnorm.rvs(a_rho, np.inf, loc=df['SNR'], scale= np.sqrt(ndet),  random_state=noise_rng)
     else:
-        df['SNR_OBS'] = df['SNR'] #set the uncertatinty so its not a function of SNR
+        df['SNR_OBS'] = df['SNR'] 
     
     # Store which original df indices passed the SNR cut
     snr_mask = df['SNR_OBS'] > detection_threshold

scripts/run_inf.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 
     assert np.all(m1s > 0) 
     assert np.all(qs > 0) 
-    assert np.all(dls > 0) 
+    #assert np.all(dls > 0) 
     assert np.all(qs<=1) 
     assert not np.any(np.isnan(pdraws)) 
     assert not np.any(np.isinf(pdraws))

src/weighting.py
@@ -267,13 +267,21 @@ def draw_mock_samples_mine(log_mc_obs, sigma_log_mc, q_obs, sigma_q,dl_true, #lo
     thetas_final= rng.choice(thetas, size=size, p=weights)
 
     scale = np.sqrt(ndet)
-    #a_rho = (0.0 - rho_obs) / scale
-    rhos = norm.rvs(loc=rho_obs, scale=scale, size=size, random_state=rng)
+    a_rho = (0.0 - rho_obs) / scale
+    rhos_0 = truncnorm.rvs(a_rho, np.inf, loc=rho_obs, scale=scale, size=2*size, random_state=rng)
+    weights =  norm.cdf(rho_obs / scale) / norm.cdf(rhos_0 / scale)
+    weights /= np.sum(weights) #normalize
+    ess = 1.0 / np.sum(weights**2)
+    if ess < size:
+        print(f"Warning: Effective sample size on rho ({ess:.1f}) < requested size ({size})")
+    rhos= rng.choice(rhos_0, size=size, p=weights)
+
+
 
     #dL = dL_fid x (Θ / Θ_fid) x ρ_fid (M, q, dL_fid, Θ_fid)  / ρ
     points = np.column_stack([m1s, qs])
     snr_fid = np.exp(rho_fun(points))
-    dls = dl_fid*thetas_final/theta_fid * snr_fid/rhos
+    dls = dl_fid*thetas_final/theta_fid * snr_fid*np.sqrt(ndet)/rhos
     
     eps=1e-30
     