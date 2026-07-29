30f76ce37ba20765251864064a29b598adad846a
Abigail Moran <amoran@rustyamd1.cm.cluster>
2026-07-29 18:15:34 -0400

    Finalized GWTC5 numbers, all working dependent on priors

--- scripts/call_rwt.sh
+++ scripts/call_rwt.sh
@@ -3,7 +3,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --mem=64G
-#SBATCH --time=0-10
+#SBATCH --time=0-02
 
 module load cuda/12 cudnn
 
@@ -12,4 +12,4 @@ unset LD_LIBRARY_PATH
 
 cd /mnt/home/amoran/GW_pop_cosmo_inference/scripts/
 
-srun python reweight_res.py --config /mnt/home/amoran/GW_pop_cosmo_inference/scripts/run_configs/mock_TC5_noevo.ini
+srun python reweight_res.py --config /mnt/home/amoran/GW_pop_cosmo_inference/scripts/run_configs/mock_O5_noevo2.ini
